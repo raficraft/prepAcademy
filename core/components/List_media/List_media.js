3df78fd77d19783a5979b2416070a2a6d9584a05
raficraft <raficraft@gmail.com>
2022-07-15 03:51:55 +0200

    add image component

--- core/components/List_media/List_media.js
+++ core/components/List_media/List_media.js
@@ -1,13 +1,18 @@
 import Link from "next/link";
-import React from "react";
+import Image from "next/image";
+import React, { useLayoutEffect, useState } from "react";
 import { IMG_URL } from "../../../config/request";
 import { Icon_heart } from "../../assets/SVG/UI_icon";
 import RadialChart from "../RadialChart/RadialChart";
 import S from "./List_media.module.scss";
 
 export default function List_media({ data, slug }) {
+  const [size_img, setSize_img] = useState({
+    x: 0,
+    y: 0,
+  });
+
   function createItems() {
-    console.log(data);
     return data.results.map((el, key) => {
       return (
         <Link
@@ -16,8 +21,14 @@ export default function List_media({ data, slug }) {
           title={el.original_title ? el.title : el.name}
         >
           <a className={S.container}>
-            <div className={S.img_container}>
-              <img src={`${IMG_URL}${el.poster_path}`} />
+            <div className={`${S.img_container} img_media`}>
+              <Image
+                objectFit="cover"
+                src={`${IMG_URL}${el.poster_path}`}
+                alt={el.original_title ? el.title : el.name}
+                width={size_img.x}
+                height={size_img.y}
+              />
               <div className={S.radial_container}>
                 <RadialChart
                   score={el.vote_average * 10}
@@ -32,7 +43,6 @@ export default function List_media({ data, slug }) {
             </div>
             <footer>
               <h4>{el.original_title ? el.title : el.name}</h4>
-
               <p
                 className={S.details}
                 onClick={(e) => {
@@ -50,5 +60,13 @@ export default function List_media({ data, slug }) {
       );
     });
   }
+
+  useLayoutEffect(() => {
+    const currentImg = document.getElementsByClassName("img_media")[0];
+    if (currentImg) {
+      const styles_img = window.getComputedStyle(currentImg);
+      setSize_img((S) => ({ ...S, x: styles_img.width, y: styles_img.height }));
+    }
+  }, []);
   return <div className={S.list_item}>{createItems()}</div>;
 }
