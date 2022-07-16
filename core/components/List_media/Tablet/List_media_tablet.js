b2bf349968882892243cf136229289be218c8b3e
raficraft <raficraft@gmail.com>
2022-07-16 19:06:26 +0200

    fix for add vercel host

--- core/components/List_media/Tablet/List_media_tablet.js
+++ core/components/List_media/Tablet/List_media_tablet.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import Image from "next/Image";
+import Image from "next/image";
 import React, { useLayoutEffect, useState } from "react";
 import { IMG_URL } from "../../../../config/request";
 import S from "./List_media_tablet.module.scss";
